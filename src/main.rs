fb322746ef07a3fea055e94037b5b0e9a287ab55
Denis Lessard <denislessard@protonmail.com>
2022-01-22 11:38:39 -0500

    Add tokio and clap parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use clap::Parser;
+use hyper::Client;
+
+#[derive(Parser)]
+struct Cli {
+    cmd: String
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let args = Cli::parse();
+
+    if (&args.cmd == "version") {
+        println!("ProductSpring CLI version 0.0.1"); // todo: reference the current git tag release
+    }
+
+    Ok(())
 }
